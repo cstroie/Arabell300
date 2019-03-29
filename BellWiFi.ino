5302b186e3db3471202d469e99b5d2138b94ff6c
Costin STROIE <cstroie@bitdefender.com>
2019-03-29 19:48:19 +0200

    Dump the EEPROM.

--- BellWiFi.ino
+++ BellWiFi.ino
@@ -37,18 +37,20 @@ HAYES hayes(&cfg, &conn);
 void setup() {
   // Serial port configuration
   Serial.begin(115200);
+  delay(100);
+  e2hex();
 
   // Define and configure the modem
-  //afsk.init(BELL103, &cfg);
+  conn.init(&cfg);
 
   // All LEDs on
   //afsk.setLeds(ON);
 
-  // Banner
-  hayes.banner();
+  // Initialize Hayes interpreter
+  hayes.init();
 
   // All LEDs off
-  delay(1000);
+  //delay(1000);
   //afsk.setLeds(OFF);
 }
 
@@ -58,9 +60,54 @@ void setup() {
 void loop() {
   // Handle TX/RX
   conn.doTXRX();
+  yield();
 
   // Check the serial port and handle data
   uint8_t sioResult = conn.doSIO();
   if      (sioResult == 255)  hayes.doSIO();
   else if (sioResult != 254)  hayes.doSIO(sioResult);
+}
+
+void e2hex() {
+  // Open EEPROM
+  EEPROM.begin(EESIZE);
+  char buf[16];
+  char val[4];
+  uint8_t data;
+  // Start with a new line
+  Serial.print("\r\n");
+  // All EEPROM bytes
+  for (uint16_t addr = 0; addr < EESIZE;) {
+    // Init the buffer
+    memset(buf, 0, 16);
+    // Use the buffer to display the address
+    sprintf_P(buf, PSTR("%04X: "), addr);
+    Serial.print(buf);
+    // Iterate over bytes, 2 sets of 8 bytes
+    for (uint8_t set = 0; set < 2; set++) {
+      for (uint8_t byt = 0; byt < 8; byt++) {
+        // Read EEPROM
+        data = EEPROM.read(addr);
+        // Prepare and print the hex dump
+        sprintf_P(val, PSTR("%02X "), data);
+        Serial.print(val);
+        // Prepare the ASCII dump
+        if (data < 32 or data > 127)
+          data = '.';
+        buf[addr & 0x0F] = data;
+        // Increment the address
+        addr++;
+      }
+      // Print a separator
+      Serial.write(' ');
+    }
+    // Print the ASCII column
+    for (uint8_t idx = 0; idx < 16; idx++) {
+      Serial.write(buf[idx]);
+    }
+    // New line
+    Serial.print("\r\n");
+  }
+  // Close EEPROM
+  EEPROM.end();
 }
